195cdccbed1a61f122db45ca0d1c2e1aec65dfb0
Akin Elden <akin.elden@boun.edu.tr>
2019-10-31 17:21:29 +0300

    ALA heuristics is completed

--- HW2_scripts.py
+++ HW2_scripts.py
@@ -3,7 +3,7 @@
 
 # # Homework 2 Solution Scripts
 
-# In[70]:
+# In[1]:
 
 
 import numpy as np
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import seaborn; seaborn.set() # Plot styling
 
 
-# In[105]:
+# In[2]:
 
 
 coordinates = pd.read_csv('coordinates_2_.csv')
@@ -20,39 +20,46 @@ demands = pd.read_csv('demand_2_.csv')
 costs = pd.read_csv('costs_2_.csv')
 
 A = np.asarray(coordinates) #first column= index of customer, second and third column= coordinates
-H = np.asarray(demands) #first column= index of customer, second column= demand
-C = np.asarray(costs)   ##first column= index of facility, "i"th columns= cost of transporting 1 unit to the customer point "i"
+H = np.abs(np.asarray(demands)) #first column= index of customer, second column= demand
+C = np.abs(np.asarray(costs))   ##first column= index of facility, "i"th columns= cost of transporting 1 unit to the customer point "i"
+A = A[:,1:]
+H = H[:,1]
 C = C[:,1:]  # index column is removed
 
 
-# In[111]:
+# In[3]:
 
 
 def squaredDistSolforSingle(H=[], A=[], C=[], m=41):
+    # If there is no assigned customer to facility, assign random location
+    if A.size == 0:
+        return np.random.randint(10,30),np.random.randint(10,30)
     facility_m = C[m] #cost vector of facility m: cost of transporting 1 unit from facility m to customer points
                                      #np.multiply(H[:,1],facility_m ->> output : element wise multiplier of cost vector and demand values
                                      #np.dot(H[:,1], facility_m) ->> output: total cost of transportation of demand
                                      #np.multiply(np.multiply(H[:,1],facility_m),A[:,1]) ->> weighted average of coordinates   
-    x_v1_star = (np.sum(np.multiply(np.multiply(H[:,1],facility_m),A[:,1]))/np.dot(H[:,1], facility_m))
-    x_v2_star = (np.sum(np.multiply(np.multiply(H[:,1],facility_m),A[:,2]))/np.dot(H[:,1], facility_m))
+    x_v1_star = (np.sum(np.multiply(np.multiply(H,facility_m),A[:,0]))/np.dot(H, facility_m))
+    x_v2_star = (np.sum(np.multiply(np.multiply(H,facility_m),A[:,1]))/np.dot(H, facility_m))
     
     return x_v1_star, x_v2_star
 
 
-# In[107]:
+# In[4]:
 
 
-plt.scatter(A[:, 1], A[:, 2], s=np.size(A,axis=0))
+plt.scatter(A[:, 0], A[:, 1], s=np.size(A,axis=0))
 
 x1, x2 = squaredDistSolforSingle(H,A,C,41)
-x1
+x1, x2
 
 
-# In[112]:
+# In[23]:
 
 
-
-def ALAHeuristics(H=[], A=[], C=[], index_array=np.arange(100), seed=440): 
+def ALAHeuristics(H=[], A=[], C=[], seed=440): 
+    cost_matrix = np.copy(C)
+    for i in range(100):
+        cost_matrix[:,i] = H[i]*C[:,i]
     # Initial step, random assignments of customers
     facility_customers = [[] for i in range(50)]
     facility_locations = np.zeros(shape=(50,2))
@@ -62,21 +69,53 @@ def ALAHeuristics(H=[], A=[], C=[], index_array=np.arange(100), seed=440):
         facility_customers[customer_assignments[i]].append(i)
     # Solving m single facility location problems and computing new objective value until no improvement
     old_objective = np.iinfo(np.int32).max
-    new_objective=0
+    count = 0
     while(True):
+        count += 1
+        new_objective = 0
         for i in range(50):
             x1, x2 = squaredDistSolforSingle(H[facility_customers[i]],A[facility_customers[i]],C[:,facility_customers[i]],i)
             facility_locations[i] = np.array([x1,x2])
-            customer_coordinates[i]=A[facility_customers[i]][:,[1,2]]
-            facility_cost=np.sum((customer_coordinates[i]-facility_locations[i])**2)
-            new_objective=new_objective+facility_cost #new objective is the cost of current assignment
-        # TODO : Make calculation
-        if(new_objective>old_objective):
+        # Calculating total cost between each facility and customer
+        total_cost_matrix = np.zeros((100,50))
+        for i in range(100):
+            coord_dif_matrix = facility_locations - A[i]
+            # for squared euclidean distance
+            distance_matrix = np.sum(coord_dif_matrix**2, axis=1)
+            total_cost_matrix[i] = np.transpose(cost_matrix[:,i])*distance_matrix
+        # New objective value calculation
+        for i in range(50):
+            facility_cost = np.sum(total_cost_matrix[facility_customers[i],i])
+            new_objective += facility_cost
+        print(new_objective)    
+        if(new_objective>=old_objective):
             break
         old_objective = new_objective
         # Reassignment of customers according to distance to facilities
-        
-    return facility_locations, objective
+        facility_customers = [[] for i in range(50)]
+        for i in range(100):
+            nearest_facility = np.argmin(total_cost_matrix[i]) # index of minimum value
+            facility_customers[nearest_facility].append(i)    
+    return facility_locations, facility_customers, old_objective, count
 
-locations, objective =ALAHeuristics(H,A,C)
+locations, assigned_customers, objective, iterations =ALAHeuristics(H,A,C)
+
+
+# In[31]:
+
+
+fig = plt.figure(figsize=(15,20))
+for i in range(50):
+    plt.title("Facility "+str(i))
+    plt.subplot(10,5,i+1)
+    plt.scatter(locations[i,0], locations[i,1], marker='^',c='r', s=60)
+    plt.scatter(A[assigned_customers[i],0], A[assigned_customers[i],1],s=20)
+    plt.tight_layout()
+
+
+# In[32]:
+
+
+plt.scatter(locations[:,0], locations[:,1], marker='^',c='r', s=60)
+plt.scatter(A[:,0], A[:,1],s=20)
 
